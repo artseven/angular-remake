e01f7c591f97de17c21d7cb5708a83e41ae27637
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-05-15 09:50:13 -0400

    error handling

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -30,6 +30,7 @@ export class ApiService {
   }
 
   private handleError (error: HttpErrorResponse) {
+    console.log(error);
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
