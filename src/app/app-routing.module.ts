5a62e00604ada8fd7f1f81016b40c6ae39eba8b3
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-05-15 11:13:25 -0400

    adding debugging to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { TestComponent } from './test/test.component';
 
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+  { path: 'test', component: TestComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full'}
+  // { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes)],
+  imports: [ RouterModule.forRoot(routes, {enableTracing: true})], // <-- for debugging purposes
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
